39b900d2381a22e19a3cf5f57a4ce4f6e0c1baf7
melissatanja <melissatanja02@users.noreply.github.com>
2018-11-30 22:10:44 -0500

    cleanup

--- leap_attempt3.js
+++ leap_attempt3.js
@@ -1,5 +1,7 @@
-var xvalues = []; 
+// var xvalues = []; 
 // var yvalues = []; 
+
+var boxes = [];
 
 //width and height for the renderer
 var w = 900;
@@ -40,6 +42,7 @@ exporter = new THREE.STLExporter();
 function leapPointToWorld(leapPoint, iBox){
 
   var normalized = iBox.normalizePoint(leapPoint, false);
+  
   var z = normalized[2];
 
   //recenter origin
@@ -59,131 +62,126 @@ function leapPointToWorld(leapPoint, iBox){
 
 function shape(){
 
-//frame is like draw, creates a loop to keep getting input from the Leap
-var frame = controller.frame();
+	//frame is like draw, creates a loop to keep getting input from the Leap
+	var frame = controller.frame();
 
-for(var f = 0; f < frame.fingers.length; f++){
+	for(var f = 0; f < frame.fingers.length; f++){
     
-	var finger = frame.fingers[f];
+		var finger = frame.fingers[f];
 
-    //0 is thumb, 1 is index, etc. 
-    //this should only let you draw with your index
-    if(finger.type == 1){
-    
-    	//variables from mapping function
-	    var iBox = frame.interactionBox;
-	    var finger_pos = leapPointToWorld(finger.tipPosition, iBox);
+	    //0 is thumb, 1 is index, etc. 
+	    //this should only let you draw with your index
+	    if(finger.type == 1){
+	    
+	    	//variables from mapping function
+		    var iBox = frame.interactionBox;
+		    var finger_pos = leapPointToWorld(finger.tipPosition, iBox);
 
-	    //constraining finger position
-	    var finger_posX = Math.min(hBoxW/2 - 15, Math.max(- (hBoxW/2 - 15), finger_pos[0]));
-	    var finger_posY = Math.min(300, Math.max(-300, finger_pos[1]));
+		    //constraining finger position
+		    var finger_posX = Math.min(hBoxW/2 - 15, Math.max(- (hBoxW/2 - 15), finger_pos[0]));
+		    var finger_posY = Math.min(300, Math.max(-300, finger_pos[1]));
 
-	    // var touchx = Math.round(finger_pos[0] * 2);
-	    // var touchy = Math.round(finger_pos[1] * 2);
+		    var touchx = Math.round(finger_posX);
+		    var touchy = Math.round(finger_posY);
 
-	    var touchx = Math.round(finger_posX);
-	    var touchy = Math.round(finger_posY);
+		    // console.log(touchx);
+		    // console.log(touchy);
 
-	    // console.log(touchx);
-	    // console.log(touchy);
+		    //circles at finger position --> line 
+		    var touch = new THREE.CircleGeometry(2);
+		    var touchPoint = new THREE.Mesh(touch);
+		    touchPoint.position.set(touchx, touchy, 0);
 
-	    var touch = new THREE.CircleGeometry(2);
-	    var touchPoint = new THREE.Mesh(touch);
-	    touchPoint.position.set(touchx, touchy, 0);
-
-	    Shape.add(touchPoint);
+		    Shape.add(touchPoint);
 
 
-	//finding edge points of renderer
-	    // var whereisleft = new THREE.CircleGeometry(25);
-	    // var left = new THREE.Mesh(whereisleft);
-	    // left.position.set(-225, 0, 0);
+			//finding edge points of renderer
+			    // var whereisleft = new THREE.CircleGeometry(25);
+			    // var left = new THREE.Mesh(whereisleft);
+			    // left.position.set(-225, 0, 0);
 
-	    // Shape.add(left);
+			    // Shape.add(left);
 
-	    // var whereisright = new THREE.CircleGeometry(25);
-	    // var right = new THREE.Mesh(whereisright);
-	    // right.position.set(225, 0, 0);
+			    // var whereisright = new THREE.CircleGeometry(25);
+			    // var right = new THREE.Mesh(whereisright);
+			    // right.position.set(225, 0, 0);
 
-	    // Shape.add(right);
+			    // Shape.add(right);
 
-	    // var whereistop = new THREE.CircleGeometry(25);
-	    // var top = new THREE.Mesh(whereistop);
-	    // top.position.set(0, 225, 0);
+			    // var whereistop = new THREE.CircleGeometry(25);
+			    // var top = new THREE.Mesh(whereistop);
+			    // top.position.set(0, 225, 0);
 
-	    // Shape.add(top);
+			    // Shape.add(top);
 
-	    // var whereisbottom = new THREE.CircleGeometry(25);
-	    // var bottom = new THREE.Mesh(whereisbottom);
-	    // bottom.position.set(0, -225, 0);
+			    // var whereisbottom = new THREE.CircleGeometry(25);
+			    // var bottom = new THREE.Mesh(whereisbottom);
+			    // bottom.position.set(0, -225, 0);
 
-		// Shape.add(bottom);
+				// Shape.add(bottom);
 
-	//horizontal box
-    var horiz = new THREE.BoxGeometry(hBoxW, 25, 25); //FIX THIS TO B AS WIDE AS THE DRAWN RECST
-    var hBox = new THREE.Mesh(horiz);
-    hBox.position.set(0, 0, 0);
+			//horizontal box
+		    var horiz = new THREE.BoxGeometry(hBoxW, 25, 25); //FIX THIS TO B AS WIDE AS THE DRAWN RECST
+		    var hBox = new THREE.Mesh(horiz);
+		    hBox.position.set(0, 0, 0);
 
-    Shape.add(hBox);
+		    Shape.add(hBox);
 
-    //keychain loop torus
-    var torus = new THREE.TorusGeometry(30, 10, 5, 20);
-    var loop = new THREE.Mesh(torus);
-    loop.position.set(-230, 0, 0);
+		    //keychain loop torus
+		    var torus = new THREE.TorusGeometry(30, 10, 5, 20);
+		    var loop = new THREE.Mesh(torus);
+		    loop.position.set(-230, 0, 0);
 
-    Shape.add(loop);
+		    Shape.add(loop);
 
-	//check if the box is equal width from other boxes
-	if(touchx % 25 === 0){
+			//check if the box is equal width from other boxes
+			if(touchx % 25 === 0){
 
-		//go through array of box x positions
-		// for(b of boxPos.children){
+				//go through array of box x positions
+				// for(b of boxPos.children){
 
-			//check if current finger x position is equal to any of the previous box x positions
-			//if not, draw a box at the current finger x position
-			
-			// if(boxPos.length == 0){
+				//check if current finger x position is equal to any of the previous box x positions
+				//if not, draw a box at the current finger x position
+					
+				// if(boxPos.length == 0){
 
-	     //      var vert = new THREE.BoxGeometry(20, touchy, touchy);
-	     //      var vBox = new THREE.Mesh(vert);
-	     //      vBox.position.set(touchx, 0, 0);
+			    //      var vert = new THREE.BoxGeometry(20, touchy, touchy);
+			    //      var vBox = new THREE.Mesh(vert);
+			    //      vBox.position.set(touchx, 0, 0);
 
-	     //      // boxPos.push(vBox.position.x);
-	     //      append(boxPos, vBox.position.x);
-	     //      console.log(boxPos);
+			    //      // boxPos.push(vBox.position.x);
+			    //      append(boxPos, vBox.position.x);
+			    //      console.log(boxPos);
 
-	     //      Shape.add(vBox);
+			    //      Shape.add(vBox);
 
-	     //      // box(20, touchy, touchy);
-	     //      // translate(-touchx, 0, 0);
-	     //  	}
+			    //      // box(20, touchy, touchy);
+			    //      // translate(-touchx, 0, 0);
+			    //  	}
 
-	      	// if(touchx != ){
+		      	// if(touchx != ){
 
-	      		//vertical boxes
-	          var vert = new THREE.BoxGeometry(15, touchy * 1.5, touchy * 1.5);
-	          var vBox = new THREE.Mesh(vert);
-	          vBox.position.set(touchx, 0, 0);
+		      	//vertical boxes
+		        var vert = new THREE.BoxGeometry(15, touchy * 1.5, touchy * 1.5);
+		        var vBox = new THREE.Mesh(vert);
+		        vBox.position.set(touchx, 0, 0);
 
-	          // var boxes = new THREE.Group();
+		        boxes.push(vBox.position.x);
 
-	          boxPos.add(vBox);
+		        boxPos.add(vBox);
 
-	          Shape.add(boxPos);
-	          // boxPos.push(vBox.position.x);
+		        Shape.add(boxPos);
 
-	          // console.log(boxPos.children);
+		        console.log(boxes);
 
-	          // Shape.add(vBox);
+		      	// }
+				// }
+	      
+	    	}
 
-	      	// }
-			// }
-      
     	}
-
-    }
     
-  }
+  	}
 
 }
 
@@ -205,7 +203,8 @@ animate();
 //keypress function to save STL file
 document.onkeydown = function(){ 
 
-  if (event.which == 13){    
+  if (event.which == 13){   
+
     let exp = exporter.parse(airPrint);
     let file = new Blob([exp], {type: 'model/stl'});
     let link = document.createElement('a');
@@ -215,6 +214,7 @@ document.onkeydown = function(){
     link.href = URL.createObjectURL(file);
     link.download = 'airPrint.stl';
     link.click();
+
   } 
  
 }
